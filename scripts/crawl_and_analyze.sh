cddaa56729dcb52045c1436f1a5fee6043303b47
bonpiedlaroute <noel.tchidjo@hotmail.com>
2019-08-13 09:30:26 +0000

    add env variable on script

--- scripts/crawl_and_analyze.sh
+++ scripts/crawl_and_analyze.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+export LD_LIBRARY_PATH=/usr/local/lib/:$LD_LIBRARY_PATH
+export PYTHONPATH=/home/ubuntu/software/thrift-0.10.0/lib/py/build/lib.linux-x86_64-3.6/:/home/ubuntu/scrapy_env/surfyn_bot/surfyn_bot/spiders:$PYTHONPATH
 cd ~/software/surfyng/sf_services/dynamodb_access/build
 ./start_dyndb
 cd ~/scrapy_env/
