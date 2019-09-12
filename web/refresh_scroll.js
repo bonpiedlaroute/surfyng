c9cc85df348067d15dbf5cd90e624e9dc20d579c
bonpiedlaroute <noel.tchidjo@hotmail.com>
2019-09-12 17:17:37 +0200

    fix scroll position between summary and details page

--- web/refresh_scroll.js
+++ web/refresh_scroll.js
@@ -1,5 +1,6 @@
 previous_link = document.getElementById("previous_link");
 previous_link.onclick = gotopreviouspage;
+var ismobile   = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent);
 
 function gotopreviouspage()
 {
@@ -10,14 +11,14 @@ function gotopreviouspage()
 
 window.onscroll=function () {
     //set scroll position in session storage
-    sessionStorage.setItem("scrollX", String(window.scrollX));
-    sessionStorage.setItem("scrollY", String(window.scrollY));
+    sessionStorage.setItem("scrollX", ismobile?String(window.pageXOffset):String(window.scrollX));
+    sessionStorage.setItem("scrollY", ismobile?String(window.pageYOffset):String(window.scrollY));
 
 };
 var init = function () {
     window.focus();
     //get scroll position in session storage and go to that scroll position
-    window.scrollTo(parseInt(sessionStorage.getItem("scrollX"), 10),
-     parseInt(sessionStorage.getItem("scrollY"), 10));
+    setTimeout(function() {window.scrollTo(parseInt(sessionStorage.getItem("scrollX"), 10),
+     parseInt(sessionStorage.getItem("scrollY"), 10)) ;}, 100);
 };
 window.onload = init;
