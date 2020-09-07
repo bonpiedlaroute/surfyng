bef002adf2802dee0e5db1c914926bad7692b76a
bonpiedlaroute <noel.tchidjo@hotmail.com>
2020-09-07 17:09:44 +0200

    adding geolocalization service

---
+++ sf_services/sf_utils/inc/Geolocal.h
@@ -0,0 +1,39 @@
+/*
+ * Created on Sun Sep 06 2020
+ *
+ * Copyright (c) 2020 Surfyn
+ */
+
+#ifndef INC_GEOLOCAL
+#define INC_GEOLOCAL
+
+#include <string>
+#include <unordered_map>
+
+namespace surfyn
+{
+namespace utils
+{
+   struct CityCode
+   {
+      std::string inseeCode{};
+      std::string postalCode{};
+   };
+   class GeoLocal {
+   private:
+      std::unordered_map<std::string, CityCode> citiesInfos;
+
+   private:
+      void init (const std::string& filename);
+   public:
+      GeoLocal() = delete;
+      GeoLocal(const std::string& csvfile);
+      std::string getInseeCode(const std::string& city) const;
+      std::string getPostalCode(const std::string& city) const;
+   };    
+}
+
+}
+
+
+#endif /* INC_GEOLOCAL */
---
+++ sf_services/sf_utils/src/Geolocal.cpp
@@ -0,0 +1,60 @@
+#include "Geolocal.h"
+#include <boost/algorithm/string/split.hpp>
+#include <string>
+#include <fstream>
+
+namespace surfyn 
+{
+namespace utils
+{
+   void GeoLocal::init(const std::string& csvfile)
+   {
+      std::fstream input;
+      input.open(csvfile);
+
+      if( input.is_open())
+      {
+         std::string line;
+         while(std::getline(input, line))
+         {
+            std::vector<std::string> results;
+            boost::split(results, line, [](char c ){ return c == ';';});
+
+            if( results.size() > 0 && results[0] == "Code INSEE")
+               continue;
+
+            if(results.size() == 3 )
+            citiesInfos.insert(std::make_pair(results[2], CityCode{results[0], results[1]}));
+         }
+      }
+      input.close();
+   }
+   GeoLocal::GeoLocal(const std::string& csvfile)
+   {
+      init(csvfile);
+   }
+   std::string GeoLocal::getInseeCode(const std::string& city) const
+   {
+      auto it = citiesInfos.find(city);
+      if( it != citiesInfos.end())
+      {
+         return it->second.inseeCode;
+      }
+      else {
+      return "";
+      }
+   }
+
+   std::string GeoLocal::getPostalCode(const std::string& city) const
+   {
+      auto it = citiesInfos.find(city);
+      if( it != citiesInfos.end())
+      {
+         return it->second.postalCode;
+      }
+      else {
+      return "";
+      }
+   }
+}
+}
