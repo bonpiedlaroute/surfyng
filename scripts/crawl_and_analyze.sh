1dffcc4884f24bfadbb0b03f9aeaaca6c9c9700e
bonpiedlaroute <noel.tchidjo@hotmail.com>
2020-09-18 00:24:21 +0200

    add agenceprincipale in crawl script

--- scripts/crawl_and_analyze.sh
+++ scripts/crawl_and_analyze.sh
@@ -20,86 +20,89 @@ do
         scrapy crawl seloger -a city="$CITY" >> "seloger_"$DATE".log" 2>&1 &
         pids[$index]=$!
         index=$((index+1))
-        sleep 7
+        sleep 3
         DATE=$(date +%Y_%m_%d"_"%H_%M_%S)
         scrapy crawl leboncoin -a city="$CITY" >> "leboncoin_"$DATE".log" 2>&1 &
         pids[$index]=$!
         index=$((index+1))
-        sleep 7
+        sleep 3
         DATE=$(date +%Y_%m_%d"_"%H_%M_%S)
         scrapy crawl bienici -a city="$CITY" >> "bienici_"$DATE".log" 2>&1 &
         pids[$index]=$!
         index=$((index+1))
-        sleep 7
+        sleep 3
         DATE=$(date +%Y_%m_%d"_"%H_%M_%S)
         scrapy crawl laforet -a city="$CITY" >> "laforet_"$DATE".log" 2>&1 &
         pids[$index]=$!
         index=$((index+1))
-        sleep 7
+        sleep 3
         DATE=$(date +%Y_%m_%d"_"%H_%M_%S)
         scrapy crawl orpi -a city="$CITY" >> "orpi_"$DATE".log" 2>&1 &
         pids[$index]=$!
         index=$((index+1))
-        sleep 7
+        sleep 3
         DATE=$(date +%Y_%m_%d"_"%H_%M_%S)
         scrapy crawl stephaneplazaimo -a city="$CITY" >> "stephaneplazaimo_"$DATE".log" 2>&1 &
         pids[$index]=$!
         index=$((index+1))
-        sleep 7
+        sleep 3
         DATE=$(date +%Y_%m_%d"_"%H_%M_%S)
         scrapy crawl foncia -a city="$CITY" >> "foncia_"$DATE".log" 2>&1 &
         pids[$index]=$!
         index=$((index+1))
-        sleep 7
+        sleep 3
         DATE=$(date +%Y_%m_%d"_"%H_%M_%S)
         scrapy crawl century21 -a city="$CITY" >> "century21_"$DATE".log" 2>&1 &
         pids[$index]=$!
         index=$((index+1))
-        sleep 7
+        sleep 3
         DATE=$(date +%Y_%m_%d"_"%H_%M_%S)
         scrapy crawl guyhoquet -a city="$CITY" >> "guyhoquet_"$DATE".log" 2>&1 &
         pids[$index]=$!
         index=$((index+1))
-        sleep 7
+        sleep 3
         DATE=$(date +%Y_%m_%d"_"%H_%M_%S)
         scrapy crawl arthurimmo -a city="$CITY" >> "arthurimmo_"$DATE".log" 2>&1 &
         pids[$index]=$!
         index=$((index+1))
-        sleep 7
+        sleep 3
         DATE=$(date +%Y_%m_%d"_"%H_%M_%S)
         scrapy crawl eraimmo -a city="$CITY" >> "eraimmo_"$DATE".log" 2>&1 &
         pids[$index]=$!
         index=$((index+1))
-        sleep 7
+        sleep 3
         DATE=$(date +%Y_%m_%d"_"%H_%M_%S)
         scrapy crawl pap -a city="$CITY" >> "pap_"$DATE".log" 2>&1 &
         pids[$index]=$!
         index=$((index+1))
-        sleep 7
+        sleep 3
         DATE=$(date +%Y_%m_%d"_"%H_%M_%S)
         scrapy crawl iadfrance -a city="$CITY" >> "iadfrance_"$DATE".log" 2>&1 &
         pids[$index]=$!
         index=$((index+1))
-        sleep 7
+        sleep 3
         DATE=$(date +%Y_%m_%d"_"%H_%M_%S)
         scrapy crawl paruvendu -a city="$CITY" >> "paruvendu_"$DATE".log" 2>&1 &
         pids[$index]=$!
         index=$((index+1))
-        sleep 7
+        sleep 3
         DATE=$(date +%Y_%m_%d"_"%H_%M_%S)
         scrapy crawl avendrealouer -a city="$CITY" >> "avendrealouer_"$DATE".log" 2>&1 &
         pids[$index]=$!
         index=$((index+1))
-        sleep 7
+        sleep 3
         DATE=$(date +%Y_%m_%d"_"%H_%M_%S)
         scrapy crawl nestenn -a city="$CITY" >> "nestenn_"$DATE".log" 2>&1 &
         pids[$index]=$!
         index=$((index+1))
+        sleep 3
+        DATE=$(date +%Y_%m_%d"_"%H_%M_%S)
+        scrapy crawl agenceprincipale -a city="$CITY" >> "agenceprincipale_"$DATE".log" 2>&1 &
+        pids[$index]=$!
         # wait for all scrapy pids
         for pid in ${pids[*]}; do
             wait $pid
         done
-        sleep 15
         deactivate
         #cd ~/software/surfyng/sf_services/sf_classifier/build/
         #./sf_classifier config_fr_properties.ini $CITY
