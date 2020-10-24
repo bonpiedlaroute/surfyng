a4c0e70ae351ec8c1a05b33f287dc44f41cb3b01
bonpiedlaroute <noel.tchidjo@hotmail.com>
2020-10-24 19:00:19 +0200

    add scroll to top button

--- web/refresh_scroll.js
+++ web/refresh_scroll.js
@@ -1,12 +1,24 @@
 var ismobile   = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent);
 
+var gotoTopButton = document.getElementById("gotoTopButton");
 
 window.onscroll=function () {
     //set scroll position in session storage
     sessionStorage.setItem("scrollX", ismobile?String(window.pageXOffset):String(window.scrollX));
     sessionStorage.setItem("scrollY", ismobile?String(window.pageYOffset):String(window.scrollY));
 
+    var currentScrollY = ismobile?window.pageYOffset:window.scrollY;
+
+    if (currentScrollY > 60 ) {
+        gotoTopButton.style.display = "block";
+    } else {
+      gotoTopButton.style.display = "none";
+    }
 };
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
 var init = function () {
     window.focus();
     //get scroll position in session storage and go to that scroll position
