f6ee2447fc07ede95c1170b8ef49d8ab72461308
bonpiedlaroute <noel.tchidjo@hotmail.com>
2020-11-05 23:04:32 +0100

    fixing image ad for avendrealouer

--- bot/avendrealouer.py
+++ bot/avendrealouer.py
@@ -158,7 +158,7 @@ class AvendreAlouerSpider(scrapy.Spider):
          data['AD_TEXT_DESCRIPTION'] = ad_text_description[0]
 
       # get images
-      images = response.xpath('//div/div/div[@class="SliderImages__DivStyledContainer-sc-18z29ar-0 enZetH"]/div/div/div/div[contains(@class, "slick-slide")]/div/div/img/@src').extract()
+      images = response.xpath('//*[@id="react_0HM416JMNK52G"]/div[2]/div[2]/div[1]/div[2]/div/div[*]/div/div/img/@src').extract()
       image_count = 1
       img_cnt = 0
       for img in images:
