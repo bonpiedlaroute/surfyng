19e537b9e85da74fa7b2fbde769b54865a5fb214
Ubuntu <ubuntu@ip-172-31-45-216.eu-west-3.compute.internal>
2020-11-12 22:08:37 +0000

    adding mail service in startup scripts

--- scripts/restart_services.sh
+++ scripts/restart_services.sh
@@ -5,8 +5,12 @@ sudo /home/ubuntu/software/surfyng/sf_services/rest_server/build/stop_rest
 sleep 2
 /home/ubuntu/software/surfyng/sf_services/dynamodb_access/build/stop_dyndb_rest
 sleep 2
+/home/ubuntu/software/surfyng/sf_services/mail/stop_mailservice
+sleep 2
 /home/ubuntu/software/surfyng/sf_services/dynamodb_access/build/start_dyndb_rest
 sleep 2
 sudo /home/ubuntu/software/surfyng/sf_services/rest_server/build/start_rest
+sleep 2
+/home/ubuntu/software/surfyng/sf_services/mail/start_mailservice
 sleep 2
 sudo /home/ubuntu/software/monit/start_monit
