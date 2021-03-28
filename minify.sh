e89c3cddb78778cc1a88ef0400246dbcfbb0384f
Stephane FEDIM <stephanefedim@gmail.com>
2021-03-28 23:37:02 +0100

    Script to minify *css and *.js added

--- minify.sh
+++ minify.sh
@@ -1,35 +1,52 @@
+#!/bin/bash
+
+# Author: Stephane Fedim
+# Description: Script to minify all css and js files before deployment
+# Tools: uglify-js & cssnano
+
+
 cd web/
 
-# Minify all *.js files
-uglifyjs --compress --mangle --verbose --webkit --output autocomplete.min.js -- autocomplete.js && cp autocomplete.min.js /var/www/html
-uglifyjs --compress --mangle --verbose --webkit --output dropdownmenu.min.js -- dropdownmenu.js && cp dropdownmenu.min.js /var/www/html
-uglifyjs --compress --mangle --verbose --webkit --output email.min.js -- email.js && cp email.min.js /var/www/html
-uglifyjs --compress --mangle --verbose --webkit --output estimate_house_price.min.js -- estimate_house_price.js && cp estimate_house_price.min.js /var/www/html
-uglifyjs --compress --mangle --verbose --webkit --output estimate_results.min.js -- estimate_results.js && cp estimate_results.min.js /var/www/html
-uglifyjs --compress --mangle --verbose --webkit --output fetch_details.min.js -- fetch_details.js && cp fetch_details.min.js /var/www/html
-uglifyjs --compress --mangle --verbose --webkit --output fetch_summary.min.js -- fetch_summary.js && cp fetch_summary.min.js /var/www/html
-uglifyjs --compress --mangle --verbose --webkit --output geolocal.min.js -- geolocal.js && cp geolocal.min.js /var/www/html
-uglifyjs --compress --mangle --verbose --webkit --output index.min.js -- index.js && cp index.min.js /var/www/html
-uglifyjs --compress --mangle --verbose --webkit --output login.min.js -- login.js && cp login.min.js /var/www/html
-uglifyjs --compress --mangle --verbose --webkit --output mysearch.min.js -- mysearch.js && cp mysearch.min.js /var/www/html
-uglifyjs --compress --mangle --verbose --webkit --output predicted_price.min.js -- predicted_price.js && cp predicted_price.min.js /var/www/html
-uglifyjs --compress --mangle --verbose --webkit --output refresh_actu.min.js -- refresh_actu.js && cp refresh_actu.min.js /var/www/html
-uglifyjs --compress --mangle --verbose --webkit --output refresh_scroll.min.js -- refresh_scroll.js && cp refresh_scroll.min.js /var/www/html
-uglifyjs --compress --mangle --verbose --webkit --output surfyn.min.js -- surfyn.js &&  cp surfyn.min.js /var/www/html
-uglifyjs --compress --mangle --verbose --webkit --output utils.min.js -- utils.js && cp utils.min.js /var/www/html
+# Create temporary folder
+mkdir tmp_minify/
 
-# Minify all *.css files
-postcss autocomplete.css > autocomplete.min.css && cp autocomplete.min.css /var/www/html
-postcss login.css > login.min.css && cp login.min.css /var/www/html
-postcss surfyn.css > surfyn.min.css && cp surfyn.min.css /var/www/html
-postcss surfyn_immo_actu.css > surfyn_immo.min.css && cp surfyn_immo.min.css /var/www/html
-postcss surfyn_immo_advice.css > surfyn_immo_advice.min.css && cp surfyn_immo_advice.min.css /var/www/html
+cp *.css tmp_minify
+cp *.js tmp_minify
 
+cd tmp_minify/
 
 IFS='.'
-for file in *.html
+# Minify all *.js files
+
+for jsfile in *.js
 do
-        for jsfile in *.js
+	read -ra ADDR <<< "$jsfile"
+	uglifyjs --compress --mangle --verbose --webkit --output "${ADDR[0]}.min.js" -- "$jsfile" && cp "${ADDR[0]}.min.js" /var/www/html
+done
+IFS=' '
+
+IFS='.'
+# Minify all *.css files
+for cssfile in *.css
+do
+	read -ra ADDR <<< "$cssfile"
+	postcss "$cssfile" > "${ADDR[0]}.min.css" && cp "${ADDR[0]}.min.css" /var/www/html
+done
+IFS=' '
+
+# Go back to web folder
+cd ../
+# Delete tmp folder
+rm -R tmp_minify/
+
+# Enter in web server folder
+# cd /var/www/html
+
+IFS='.'
+# Replace all *.js files in /var/www/html/*.html files by *.min.js
+for file in /var/www/html/*.html
+do
+        for jsfile in web/*.js
         do
                 if grep "$jsfile" "$file"
                 then
@@ -38,11 +55,13 @@ do
                 fi
         done
 done
+IFS=' '
 
 IFS='.'
-for file in *.html
+# Replace all *.css files in /var/www/html*.html files by *.min.css
+for file in /var/www/html/*.html
 do
-        for cssfile in *.css
+        for cssfile in web/*.css
         do
                 if grep "$cssfile" "$file"
                 then
@@ -52,7 +71,3 @@ do
         done
 done
 IFS=' '
-
-# Set the ownership to the current user
-chown -R $(id -u):$(id -g) *.min.js
-chown -R $(id -u):$(id -g) *.min.css
