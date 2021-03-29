af717c72534ac0786c74cc78f8a2b66fe9e1ff4c
bonpiedlaroute <noel.tchidjo@hotmail.com>
2021-03-29 18:20:39 +0200

    adapting for production readiness

--- tools/minify/minify.sh
+++ tools/minify/minify.sh
@@ -4,14 +4,13 @@
 # Description: Script to minify all css and js files before deployment
 # Tools: uglify-js & cssnano
 
-
-cd web/
-
 # Create temporary folder
-mkdir tmp_minify/
+mkdir -p tmp_minify/
 
-cp *.css tmp_minify
-cp *.js tmp_minify
+
+cp /home/ubuntu/software/surfyng/web/*.css tmp_minify
+cp /home/ubuntu/software/surfyng/web/*.js tmp_minify
+
 
 cd tmp_minify/
 
@@ -37,7 +36,7 @@ IFS=' '
 # Go back to web folder
 cd ../
 # Delete tmp folder
-rm -R tmp_minify/
+rm -rf tmp_minify/
 
 # Enter in web server folder
 # cd /var/www/html
@@ -46,10 +45,13 @@ IFS='.'
 # Replace all *.js files in /var/www/html/*.html files by *.min.js
 for file in /var/www/html/*.html
 do
-        for jsfile in web/*.js
+        echo "looking for $file replacement"
+        cd /home/ubuntu/software/surfyng/web/
+        for jsfile in *.js
         do
-                if grep "$jsfile" "$file"
-                then
+                grep "$jsfile" "$file"
+                if [ $? -eq 0 ]; then
+                        echo "replacing $jsile in $file"
                         read -ra ADDR <<< "$jsfile"
                         sed -i "s/$jsfile/${ADDR[0]}.min.js/g" "$file"
                 fi
@@ -61,10 +63,13 @@ IFS='.'
 # Replace all *.css files in /var/www/html*.html files by *.min.css
 for file in /var/www/html/*.html
 do
-        for cssfile in web/*.css
+        echo "looking for $file replacement"
+        cd /home/ubuntu/software/surfyng/web/
+        for cssfile in *.css
         do
-                if grep "$cssfile" "$file"
-                then
+                grep "$cssfile" "$file"
+                if [ $? -eq 0 ];then
+                        echo "replacing $cssfile in $file"
                         read -ra ADDR <<< "$cssfile"
                         sed -i "s/$cssfile/${ADDR[0]}.min.css/g" "$file"
                 fi
