9df8ff1df2069a981ede013da2643714001144d6
Ubuntu <ubuntu@ip-172-31-45-216.eu-west-3.compute.internal>
2021-04-04 15:20:43 +0000

    adapting launching order

--- scripts/restart_services.sh
+++ scripts/restart_services.sh
@@ -10,15 +10,19 @@ sleep 2
 /home/ubuntu/software/surfyng/sklearn/stop_predictor
 sleep 2
 /home/ubuntu/software/surfyng/sf_services/mail/stop_mailservice
-sleep 2
-/home/ubuntu/software/surfyng/sf_services/dynamodb_access/build/start_dyndb_rest
+#sleep 2
+#/home/ubuntu/puppeteer_workdir/stop_puppeteer.sh
 sleep 2
 /home/ubuntu/software/surfyng/sklearn/start_predictor
+sleep 2
+/home/ubuntu/software/surfyng/sf_services/dynamodb_access/build/start_dyndb_rest
 sleep 2
 /home/ubuntu/software/surfyng/sf_services/emailalertservice/start_emailalert.sh
 sleep 2
 sudo /home/ubuntu/software/surfyng/sf_services/rest_server/build/start_rest
 sleep 2
 /home/ubuntu/software/surfyng/sf_services/mail/start_mailservice
+#sleep 2
+#/home/ubuntu/puppeteer_workdir/start_puppeteer.sh
 sleep 2
 sudo /home/ubuntu/software/monit/start_monit
