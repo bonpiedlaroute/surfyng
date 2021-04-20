037c640e250e3b73043a4b4844396ec2b196ea91
Stephane Fedim <stephanefedim@gmail.com>
2021-04-20 23:53:58 +0100

    Upload image with status OK

--- web/upload.js
+++ web/upload.js
@@ -7,12 +7,13 @@ $(function() {
 		if (/^image/.test(files[0].type)){
 			var reader = new FileReader();
 			reader.readAsDataURL(files[0]);
-
+			
 			reader.onloadend = function(){
 				$("#imagePreview1").css("background-image", "url("+this.result+")");
 				$("#imagePreview1").css("background-color", "");
 				$("#icon1").css("display", "none");
 				$("#span1").css("display", "none");
+				$("#overlay-1").css("display", "flex");
 			}
 		}
 	});
@@ -30,6 +31,7 @@ $(function() {
 				$("#imagePreview2").css("background-image", "url("+this.result+")");
 				$("#imagePreview2").css("background-color", "");
 				$("#icon2").css("display", "none");
+				$("#overlay-2").css("display", "flex");
 			}
 		}
 	});
@@ -47,6 +49,7 @@ $(function() {
 				$("#imagePreview3").css("background-image", "url("+this.result+")");
 				$("#imagePreview3").css("background-color", "");
 				$("#icon3").css("display", "none");
+				$("#overlay-3").css("display", "flex");
 			}
 		}
 	});
@@ -64,6 +67,7 @@ $(function() {
 				$("#imagePreview4").css("background-image", "url("+this.result+")");
 				$("#imagePreview4").css("background-color", "");
 				$("#icon4").css("display", "none");
+				$("#overlay-4").css("display", "flex");
 			}
 		}
 	});
@@ -72,32 +76,15 @@ $(function() {
 $("#imagePreview1").click(function() {
 	$("#uploadImage1").click();
 });
-$("#icon1").click(function() {
-	$("#uploadImage1").click();
-});
-$("#span1").click(function() {
-	$("#uploadImage1").click();
-});
-
 
 $("#imagePreview2").click(function() {
 	$("#uploadImage2").click();
 });
-$("#icon2").click(function() {
-	$("#uploadImage2").click();
-});
-
 
 $("#imagePreview3").click(function() {
 	$("#uploadImage3").click();
 });
-$("#icon3").click(function() {
-	$("#uploadImage3").click();
-});
 
 $("#imagePreview4").click(function() {
-	$("#uploadImage4").click();
-});
-$("#icon4").click(function() {
 	$("#uploadImage4").click();
 });
