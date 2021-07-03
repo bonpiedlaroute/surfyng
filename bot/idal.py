f1202ec19edf09811982be610db9cbcca92d46b2
Stephane FEDIM <stephanefedim@gmail.com>
2021-07-03 11:28:42 +0100

    Update: APIMO/Idal retrieve available

--- bot/idal.py
+++ bot/idal.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*- 
-# (c) Copyright 2017-2021 
+# -*- coding: utf-8 -*-
+# (c) Copyright 2017-2021
 # author(s): Stephane Fedim
 # All rights reserved
-
 
 
 from hash_id import hash_id
@@ -12,7 +11,7 @@ from search_features import getEfficityPropertiesId, APART_ID, HOUSE_ID, BUY_ID
 from unidecode import unidecode
 from Serializer import Serializer
 from requests.auth import HTTPBasicAuth
-
+from pprint import pprint
 
 import os
 import sys
@@ -36,27 +35,110 @@ user = config_idal['APIMO']['provider']
 password = config_idal['APIMO']['token']
 
 response = requests.get(url, auth=HTTPBasicAuth(user, password))
+units_response = requests.get(
+    'https://api.apimo.pro/catalogs/unit_area', auth=HTTPBasicAuth(user, password))
+heating_response = requests.get(
+    'https://api.apimo.pro/catalogs/property_heating_type', auth=HTTPBasicAuth(user, password))
+heating_access_response = requests.get(
+    'https://api.apimo.pro/catalogs/property_heating_access', auth=HTTPBasicAuth(user, password))
+floor_response = requests.get(
+    'https://api.apimo.pro/catalogs/property_floor', auth=HTTPBasicAuth(user, password))
 
 data = response.json()
 
+
 def main():
-	city =  sys.argv[1]
-	region = config_idal[city.upper()]['region']
+    city = sys.argv[1]
+    region = config_idal[city.upper()]['region']
 
-	announces_count = data['total_items']
-	serializer = Serializer(ip, port, tablename)
-	ads = serializer.scanidByCityAndAdSource(city, "idal")
+    announces_count = data['total_items']
+    serializer = Serializer(ip, port, tablename)
+    ads = serializer.scanidByCityAndAdSource(city, "idal")
 
-	announces = dict()
-	for ad in data['properties']:
-		# Region
-		if ad['region']['name'] = 'Hauts-de-Seine':
-			pass
+    announce = dict()
+
+    for ad in data['properties']:
+        region = ad['region']['name']
+        language = ad['comments']['language']
+        prop_type = ad['type']
+        search_type = ad['category']
+
+        search_filter = region == 'Hauts-de-Seine' and language == 'fr' and prop_type in (
+            1, 2) and search_type in (1, 2)
+
+        # Region
+        if search_filter:
+			announce['ID'] = hash_id(str(ad['id']) + ad['pictures'][0]['url'])
+			announce['ANNOUNCE_TITLE'] = ad['comments']['title']
+			announce['ANNOUNCE_SOURCE'] = 'idal/apimo'
+			announce['CITY'] = ad['city']['name']
+			announce['AD_TEXT_DESCRIPTION'] = ad['comments']['comment']
+			announce['PROPERTY_TYPE'] = ad['type']
+			announce['REGION'] = region
+			announce['SEARCH_TYPE'] = search_type
+			announce['ANNOUNCE_IMAGE'] = ad['pictures'][0]['url']
+			# Get images
+			images = {}
+			for picture in ad['pictures']:
+				images['image{}'.format(picture['rank'])] = picture['url']
+			announce['IMAGE'] = str(images)
+			announce['PRICE'] = ad['price']['value']
+			# For surface we must consult units_response to get the right unit of area
+			announce['SURFACE'] = ad['area']['value']
+			announce['ROOMS'] =  ad['rooms']
+			announce['BEDROOMS'] = ad['bedrooms']
+			
+			if ad['construction']['construction_year']:
+				announce['CONSTRUCTION_YEAR'] = ad['construction']['construction_year']
+			if ad['heating']['type']:
+				heating = heating_response[ad['heating']['type']-1]['name']
+				announce['TYPE_OF_HEATING'] =  heating
+				if ad['heating']['access']:
+					access = heating_access_response[ad['heating']['access']-1]['name']
+					heating += ' {}'.format(access)
+			# Get cave
+			cellars = 0
+			for area in ad['areas']:
+				if area['type'] == 6:
+					cellars += 1
+			if cellars:
+				announce['CELLAR'] =  str(cellars)
+			# Get floor
+			if ad['floor']['value']:
+				announce['FLOOR'] = ad['floor']['value']
+			
+			# Get parking
+			parkings = 0
+			for area in ad['areas']:
+				if area['type'] == 5:
+					parkings += 1
+			if parkings:
+				announce['PARKING'] = str(parkings)
+			
+			if prop_type == 2:
+				announce['LAND_SURFACE'] = ad['area']['total']
+
+			# Get balcony
+			balcony = False
+			for area in ad['areas']:
+				if area['type'] == 43:
+					balcony = True
+					break
+			if balcony:
+				announce['BALCONY'] = 'Oui'
+			else:
+				announce['BALCONY'] = 'Non'
+			
+			if ad['address']:
+				announce['ADDRESS'] = ad['address']
+			if ad['longitude'] and ad['latitude']:
+				announce['LOCATION'] = "{}, {}".format(ad['longigute'], ad['latitude'])
+			pprint(announce)
 
 
 
 if __name__ == '__main__':
-	main()
+    main()
 
 # class EfficitySpider(scrapy.Spider):
 # 	name = 'efficity'
@@ -68,14 +150,14 @@ if __name__ == '__main__':
 
 # 		if not os.path.exists(self.images_folder_name):
 # 			os.mkdir(self.images_folder_name)
-		
+
 # 		self.announces_count = 0
 # 		self.serializer = Serializer(ip, port, tablename)
 # 		self.ads = self.serializer.scanidByCityAndAdSource(city, "efficity")
 
 # 	def start_requests(self):
 # 		prop_list = [APART_ID, HOUSE_ID]
-		
+
 # 		for ptype in prop_list:
 # 			announcers_id = getEfficityPropertiesId(ptype)
 
@@ -84,7 +166,7 @@ if __name__ == '__main__':
 
 # 	def parse(self, response, ptype, nextpage):
 # 		base_url = 'https://www.efficity.com'
-		
+
 # 		links = response.css('.programs-list-item-visual a::attr("href")').getall()
 
 # 		for link in links:
@@ -95,14 +177,14 @@ if __name__ == '__main__':
 # 				yield scrapy.Request(url=true_link, callback=lambda response, url=true_link, ptype=ptype: self.parse_announce(response, url, ptype))
 # 			else:
 # 				self.serializer.updateTimeStamp(ID)
-		
+
 # 		href = response.xpath('/html/body/div[2]/ul/li[3]/a/@href').get()
 # 		if nextpage == 1:
 # 			if href is not None:
 # 				next_link = base_url + href
 # 			else:
 # 				return
-				
+
 # 			if next_link:
 # 				yield scrapy.Request(url=next_link, callback=lambda response, ptype=ptype: self.parse(response, ptype, 2))
 # 		else:
@@ -127,7 +209,7 @@ if __name__ == '__main__':
 # 			data['PRICE'] = price.encode('ascii', 'ignore').replace('*', '').strip()
 # 		else:
 # 			return
-		
+
 # 		area = extract_css_query('h1.program-details-title::text')
 # 		if area:
 # 			data['SURFACE'] = area.split('-')[1].encode('ascii', 'ignore').replace('m', '').strip()
@@ -159,7 +241,7 @@ if __name__ == '__main__':
 
 # 		parsed_features = {}
 # 		linear_features = []
-		
+
 # 		# parsing
 # 		for feature in features:
 # 			data_splited = feature.split(':')
@@ -176,7 +258,7 @@ if __name__ == '__main__':
 # 			if 'origine du chauffage' in key.lower():
 # 				heating = parsed_features[key]
 # 				data['TYPE_OF_HEATING'] = heating
-			
+
 # 			if 'type du chauffage' in key.lower():
 # 				heating += ' + {}'.format(parsed_features[key])
 # 				data['TYPE_OF_HEATING'] = heating
